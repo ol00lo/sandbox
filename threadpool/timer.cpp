65150afb7b4f0ec102a6cb81063e22e2026c1977
ol00lo <mymrina2002liza@mail.ru>
2024-07-15 10:08:12 +0300

    threadpool

---
+++ threadpool/timer.cpp
@@ -0,0 +1,28 @@
+#include "timer.h"
+#include <chrono>
+
+Timer::Timer() {
+	_pimpl = std::make_shared<Impl>();
+}
+
+class Timer::Impl {
+public:
+	void start() {
+		_t = std::chrono::high_resolution_clock().now();
+	}
+
+	int end() {
+		auto t = std::chrono::high_resolution_clock().now();
+		return std::chrono::duration_cast<std::chrono::milliseconds>(t - _t).count();
+	}
+private:
+	std::chrono::high_resolution_clock::time_point _t;
+};
+
+void Timer::start() {
+	_pimpl->start();
+}
+
+int Timer::end() {
+	return _pimpl->end();
+}
