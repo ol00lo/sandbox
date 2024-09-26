e779f944fbe4341aad0cd218ba83f9e7aa46742f
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2024-09-26 14:08:32 +0300

    try{}catch(){}

--- life/main.cpp
+++ life/main.cpp
@@ -1,28 +1,35 @@
-//#include "display.hpp"
-#include"game.hpp"
-//#include <SFML/Graphics.hpp>
+// #include "display.hpp"
+#include "game.hpp"
+// #include <SFML/Graphics.hpp>
+#include <chrono>
 #include <iostream>
 #include <string>
-#include <chrono>
 #include <thread>
 
 int main(int argc, char* argv[])
 {
-    int width = std::stoi(argv[1]);
-    int height = std::stoi(argv[2]);
-    int norganisms = std::stoi(argv[3]);
-    width = 15;
-    height = 17;
-    norganisms = 40;
-    GameOfLife game(height, width);
-    // game.initialize({1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0});
-    game.random_initialize(norganisms);
-
-    game.display();
-    while (game.step())
+    try
     {
+        int width = std::stoi(argv[1]);
+        int height = std::stoi(argv[2]);
+        int norganisms = std::stoi(argv[3]);
+        width = 15;
+        height = 17;
+        norganisms = 40;
+        GameOfLife game(height, width);
+        // game.initialize({1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0});
+        game.random_initialize(norganisms);
+
         game.display();
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        while (game.step())
+        {
+            game.display();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+        game.over();
     }
-    game.over();
+    catch (std::exception e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
