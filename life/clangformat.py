ddb9eed1752c804b7fca1ffea88191b24bdab24a
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2024-10-09 11:16:09 +0300

    .

--- life/clangformat.py
+++ life/clangformat.py
@@ -3,27 +3,18 @@ import os
 
 directory = os.path.dirname(os.path.abspath(__file__))
 
-neededfiles = []
-if os.name == "nt":
-    exefile = os.path.join(directory, "build", "Debug", "simlife.exe")
-elif os.name == "posix":
-    exefile = os.path.join(directory, "build", "Makefile")
-    
-exetime = os.path.getmtime(exefile) 
-
 for root, dirs, files in os.walk(directory):
     if 'build' in dirs:
         dirs.remove('build')
     for file in files:
-        if file.endswith('.cpp') or file.endswith('.hpp'):
-            filetime = os.path.getmtime(file)
-            if filetime > exetime:
-                neededfiles.append(file)
-
-
-for file in neededfiles:
-    print(file)
-    os.system(f"clang-format -i {file}") 
+        if file.endswith('.cpp') or file.endswith('.hpp') or file.endswith('.h'):
+            command = f'clang-format {file}'
+            modfile = os.popen(command).read()
+            with open(file) as f:
+                curfile = f.read()
+            if curfile!= modfile:
+                os.system(f"clang-format -i {file}")
+                print(f"File  {file}  has changed") 
 
 print("DONE")
 time.sleep(2)
