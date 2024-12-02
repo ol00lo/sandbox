d33209154f51038e1e01b51757319f104fb9b72f
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2024-12-02 21:37:34 +0300

    .

--- img_classification/augmenter.py
+++ img_classification/augmenter.py
@@ -12,6 +12,7 @@ np.bool = bool
 def get_augmenter(level):
     if level == 0:
         return iaa.Sequential([
+            iaa.Sometimes(0.05, iaa.Noop()), 
             iaa.SomeOf((1, None), [
                 iaa.Fliplr(0.5),
                 iaa.Affine(rotate=(-20, 20), mode='reflect'),  
@@ -22,11 +23,11 @@ def get_augmenter(level):
                 ]),
                 iaa.AddToHue((-50, 50)),
             ], random_order=True),
-            iaa.Sometimes(0.05, iaa.Noop())  
         ])
         
     elif level == 1:
         return iaa.Sequential([
+            iaa.Sometimes(0.05, iaa.Noop()), 
             iaa.SomeOf((2, 5), [
                 iaa.Fliplr(0.5),
                 iaa.Affine(rotate=(-20, 20), mode='reflect'), 
@@ -36,11 +37,11 @@ def get_augmenter(level):
                 iaa.MotionBlur(k=(3, 5)),  
                 iaa.JpegCompression(compression=(80, 99)),  
                 iaa.Rain(speed=(0.1, 0.3))
-            ], random_order=True),
-            iaa.Sometimes(0.05, iaa.Noop())
+            ], random_order=True)
         ])
     elif level == 2:
         return iaa.Sequential([
+            iaa.Sometimes(0.05, iaa.Noop()), 
             iaa.SomeOf((3, 7), [
                 iaa.Fliplr(0.5),
                 iaa.Affine(rotate=(-20, 20), mode='reflect'),
@@ -51,11 +52,10 @@ def get_augmenter(level):
                 iaa.PiecewiseAffine(scale=(0.01, 0.05)),
                 iaa.JpegCompression(compression=(80, 99)),  
                 iaa.CropAndPad(percent=(-0.15, 0.15), pad_mode=ia.ALL),
-                iaa.CoarseDropout(0.02, size_percent=0.15, per_channel=0.5),
+                iaa.CoarseDropout((0.1, 0.2), size_percent=(0.02, 0.06), per_channel=0.5),
                 iaa.GammaContrast(gamma=(0.5, 2.0)),
                 iaa.imgcorruptlike.Snow(severity=2)
-            ], random_order=True),
-            iaa.Sometimes(0.05, iaa.Noop())
+            ], random_order=True)
         ])
     else:
         raise ValueError(":((")
@@ -78,20 +78,20 @@ def prog(image_files, nbatch, level, img_size):
     
     return selected_files, images_aug, load_times
 
-def display_images(selected_files, images, labels):
+def display_images(selected_files, images, labels, img_size):
     images = images[:4]
     if len(images) == 1:
         image = images[0]
     elif len(images) == 2:
-        image = np.zeros((128, 256, 3), dtype=np.uint8) 
-        image[:, :128] = images[0]
-        image[:, 128:] = images[1] 
+        image = np.zeros((img_size, 2*img_size, 3), dtype=np.uint8) 
+        image[:, :img_size] = images[0]
+        image[:, img_size:] = images[1] 
     else:
-        image = np.zeros((2 * 128, 2 * 128, 3), dtype=np.uint8)
+        image = np.zeros((2 * img_size, 2 * img_size, 3), dtype=np.uint8)
         for idx, img in enumerate(images):
             row = idx // 2
             col = idx % 2
-            image[row * 128:(row + 1) * 128, col * 128:(col + 1) * 128] = img
+            image[row * img_size:(row + 1) * img_size, col * img_size:(col + 1) * img_size] = img
 
     cv2.imshow('SPACE - next; ESC - exit', image)
 
@@ -112,7 +112,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--datadir', type=str, required=True, help='Directory with images and annotations')
 parser.add_argument('--nbatch', type=int, default=4, help='Batch size (default: 4)')
 parser.add_argument('--level', type=int, choices=[0, 1, 2], default=0, help='Augmentation level (0, 1, 2)')
-parser.add_argument('--img_size', type=int, default=128, help='Size for resizing images (default: 128)')  # Новый аргумент
+parser.add_argument('--img_size', type=int, default=128, help='Size for resizing images (default: 128)') 
 args = parser.parse_args()
 
 
@@ -151,7 +151,7 @@ while True:
         total_img_load_time += sum(load_times)
         img_count += len(load_times)
 
-        display_exit = display_images(selected_files, images_aug, labels)
+        display_exit = display_images(selected_files, images_aug, labels, img_size)
         if display_exit: 
             elapsed_time = (total_img_load_time) / img_count * 1000
             print(f"END... Average image loading time: {elapsed_time:.4f} milliseconds.")
