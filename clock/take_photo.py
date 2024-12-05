c50be224762dfb8c3fbd13ff835c0f2d647d092b
ek <kalininei@yandex.ru>
2024-12-05 03:10:53 +0300

    camera to 1280x720

--- clock/take_photo.py
+++ clock/take_photo.py
@@ -8,6 +8,8 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 if not cap.isOpened():
     print("Error: Failed to open the camera.")
