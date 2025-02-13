adb272cd60234698060fb31b6a90bca257a92633
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2025-02-13 17:26:28 +0300

    LIZA-118: Рациональные дроби на этапе компилляции

---
+++ templates/ratio/ratio.cpp
@@ -0,0 +1,23 @@
+﻿#include "ratio.hpp"
+
+int main()
+{
+    using t1 = Ratio<1, 4>;
+    using t2 = Ratio<7, 6>;
+    using t3 = Add<t1, t2>::type;
+    using t4 = Mult<t1, t2>::type;
+    using t11 = Simplify<t3>::type;
+
+    std::cout << "34/24 == " << t3::str() << std::endl;
+    std::cout << "17/12 == " << t11::str() << std::endl;
+    std::cout << "34/24 == " << t3::str() << std::endl;
+    std::cout << "7/24 == " << t4::str() << std::endl;
+    std::cout << "0 == " << Compare<Ratio<1, 3>, Ratio<2, 6>>::value << std::endl;
+    std::cout << "-1 == " << Compare<Ratio<-1, 3>, Ratio<2, 6>>::value << std::endl;
+    std::cout << "1 == " << Compare<Ratio<1, 3>, Ratio<-2, 6>>::value << std::endl;
+
+    std::cout << Ratio<-1, 3>::str() << std::endl;
+    std::cout << Ratio<1, -3>::str() << std::endl;
+    std::cout << Ratio<-1, -3>::str() << std::endl;
+    std::cout << Simplify<Ratio<-9, -3>>::type::str() << std::endl;
+}
---
+++ templates/ratio/ratio.hpp
@@ -0,0 +1,72 @@
+﻿#ifndef RATIO_HPP
+#define RATIO_HPP
+#include <iostream>
+#include <string>
+
+template <int A, int B>
+constexpr int GCD()
+{
+    if constexpr (B == 0)
+    {
+        return A;
+    }
+    else
+    {
+        return GCD<B, A % B>();
+    }
+}
+
+template <int Num, int Denum>
+struct Ratio
+{
+    static_assert(Denum != 0, "Denominator cannot be zero");
+
+    static constexpr int num = Denum < 0 ? -Num : Num;
+    static constexpr int denum = Denum <0 ? -Denum : Denum;
+
+    static std::string str()
+    {
+        return std::to_string(num) + "/" + std::to_string(denum);
+    }
+};
+
+template <class R>
+struct Simplify
+{
+    static constexpr int gcd = GCD<R::num, R::denum>();
+    using type = Ratio<R::num / gcd, R::denum / gcd>;
+};
+
+template <class R1, class R2>
+struct Add
+{
+    using type = Ratio<R1::num * R2::denum + R2::num * R1::denum, R1::denum * R2::denum>;
+};
+
+template <class R1, class R2>
+struct Sub
+{
+    using type = Ratio<R1::num * R2::denum - R2::num * R1::denum, R1::denum * R2::denum>;
+};
+
+template <class R1, class R2>
+struct Mult
+{
+    using type = Ratio<R1::num * R2::num, R1::denum * R2::denum>;
+};
+
+template <class R1, class R2>
+struct Div
+{
+    static_assert(R2::num != 0, "Cannot divide by zero");
+    using type = Ratio<R1::num * R2::denum, R1::denum * R2::num>;
+};
+
+template <class R1, class R2>
+struct Compare
+{
+    static constexpr int value = (R1::num * R2::denum < R2::num * R1::denum)   ? -1
+                                 : (R1::num * R2::denum > R2::num * R1::denum) ? 1
+                                                                               : 0;
+};
+#endif
