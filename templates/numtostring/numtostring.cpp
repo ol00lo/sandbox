4cf55799888c5223723ba7d417669e2df2402fb9
ol00lo <115536900+ol00lo@users.noreply.github.com>
2025-02-26 14:46:33 +0300

    [LIZA-120] Шаблон перевода числа в строку: произовольное кол-во знаков (#39)

---
+++ templates/numtostring/numtostring.cpp
@@ -0,0 +1,33 @@
+﻿#include "numtostring.hpp"
+#include "long_scale_nums.hpp"
+// #include "short_scale_nums.hpp"
+#include <iostream>
+#ifdef _WIN32
+#include "Windows.h"
+#endif
+
+//template <>
+//struct num::DigitTraits<2>
+//{
+//    static constexpr std::string_view name = "сто";
+//};
+
+using namespace num;
+
+int main()
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    setlocale(LC_ALL, "ru");
+
+    std::cout << num::NumToString<2, 2, 3, 3, 4, 5, 6>::apply() << std::endl;
+    std::cout << num::NumToString<1, 5, 6, 2, 9, 8, 2, 2, 3, 1, 4, 5, 6>::apply() << std::endl;
+    std::cout << num::NumToString<1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0>::apply() << std::endl;
+    std::cout << num::NumToString<1, 0, 0, 0, 0, 0, 0>::apply() << std::endl;
+    std::cout << num::NumToString<0, 0, 0, 5>::apply() << std::endl;
+    std::cout << num::Int<-453>::str() << std::endl;
+
+    return 0;
+}
