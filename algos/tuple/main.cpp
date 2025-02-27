1109b53dc70cfcbaf1732a7aa531540b4066c66b
ol00lo <115536900+ol00lo@users.noreply.github.com>
2025-02-27 15:01:25 +0300

    [LIZA-121] Алгоритмы над std::tuple (#41)

---
+++ algos/tuple/main.cpp
@@ -0,0 +1,103 @@
+﻿#include "tuple_algos.hpp"
+
+void my_print(int v)
+{
+    std::cout << "int " << v << std::endl;
+};
+void my_print(double v)
+{
+    std::cout << "double " << v << std::endl;
+};
+
+void my_print(bool v)
+{
+    std::cout << "bool " << v << std::endl;
+};
+
+int transform_entry(double x)
+{
+    return (int)x;
+}
+bool transform_entry(int x)
+{
+    return (bool)x;
+}
+int transform_entry(float x)
+{
+    return 2 * (int)x;
+}
+struct Functor
+{
+    int ncalls = 0;
+    void operator()(int x)
+    {
+        x *= 2;
+        ncalls += 1;
+    }
+};
+
+class A
+{
+public:
+    A(int v)
+    {
+        a = 2 * v;
+    }
+    A() = delete;
+    int a;
+};
+A transform_entry(A a)
+{
+    return a.a*=2;
+}
+
+int main()
+{
+    std::cout << "for each:\n\n";
+    using t_t = std::tuple<int, double, int>;
+    t_t tt{1, 2.5, 3};
+    auto set_zero = [](auto& v) { v = 0; };
+    auto set_one = [](auto& v) { v = 1; };
+    std::cout << "before: \nint " << std::get<0>(tt) << " double " << std::get<1>(tt) << " int " << std::get<2>(tt)
+              << std::endl;
+    for_each(tt, set_zero);
+    std::cout << "\nafter set 0: \n";
+    for_each(tt, [](auto v) { my_print(v); });
+    for_each_fe(tt, set_one);
+    std::cout << "\nafter set 1: \n";
+    for_each_fe(tt, [](auto v) { my_print(v); });
+    Functor f;
+    for_each(tt, f);
+    std::cout << f.ncalls << " == 3\n";
+    for_each(tt, Functor());
+
+
+    std::cout << "\nany of:\n\n";
+    std::tuple<double, double, int> t1{1.0, -2.0, 6};
+    bool r = any_of(t1, [](auto v) -> bool { return v < 0; });
+    std::cout << std::boolalpha << "true == " << r << std::endl;
+    r = any_of_fe(t1, [](auto v) -> bool { return v < 0; });
+    std::cout << std::boolalpha << "true == " << r << std::endl;
+    r = any_of(t1, [](auto v) -> bool { return v < -3; });
+    std::cout << std::boolalpha << "false == " << r << std::endl;
+
+    std::cout << "\ntransform:\n\n";
+    auto op = [](const auto& v) { return transform_entry(v); };
+
+    using in_t = std::tuple<float, double, int>;
+    in_t h{1.f, 1.0, 1};
+    std::cout << "type before transform: " << type_name<in_t>() << std::endl;
+    using out_t = decltype(transform(h, op));
+    std::cout << "after transform should be: std::tuple<int,int,bool>" << std::endl;
+    std::cout << "result: " << type_name<out_t>() << std::endl;
+    out_t t = transform(h, op);
+    std::cout << "\nresult should be:\nint 2\nint 1\nbool 1\n\nresult: \n";
+    for_each(t, [](auto v) { my_print(v); });
+    std::cout << "\nresult fe:\n";
+    out_t t2 = transform_fe(h, op);
+    for_each(t2, [](auto v) { my_print(v); });
+
+    std::tuple<A, int, bool> t3{A(1), 2, true};
+    transform_fe(t3, op);
+    //transform(t3, op);
+}
