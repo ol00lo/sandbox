2dbab18c824de119d2179bd938872da71529323b
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2025-02-27 14:53:42 +0300

    LIZA-123: Операции над integer_sequence

---
+++ algos/integer_sequence/is.hpp
@@ -0,0 +1,72 @@
+#ifndef IS_HPP
+#define IS_HPP
+
+#include <iostream>
+#include <array>
+#include <utility>
+
+// ========= operator<<
+template <typename T, T... I>
+std::ostream& operator<<(std::ostream& os, const std::integer_sequence<T, I...>&)
+{
+    os << "< ";
+    ((os << I <<  " "), ...);
+    os << ">";
+    return os;
+}
+
+// ========= make sequence
+template <int Start, int End, int Step>
+using is_valid = typename std::enable_if<(Step>0 ? Start<=End : End<=Start)>::type;
+
+template <int Start, int End, int Step>
+using is_end = typename std::enable_if<(Step>0 ? Start>End : End>Start)>::type;
+
+template <int Step>
+using is_step = typename std::enable_if<Step == 0>::type;
+
+template <typename Enable = void, int Start = 0, int End = 0, int Step = 0, int... Indices>
+struct make_sequence_impl;
+
+template <int Start, int End, int Step, int... Indices>
+struct make_sequence_impl<is_valid<Start, End, Step>, Start, End, Step, Indices...>
+    : make_sequence_impl<void, Start + Step, End, Step, Indices..., Start>
+{};
+
+template <int Start, int End, int Step, int... Indices>
+struct make_sequence_impl<is_end<Start, End, Step>, Start, End, Step, Indices...>
+{
+    using type = std::integer_sequence<int, Indices...>;
+};
+
+template <int Start, int End>
+struct make_sequence_impl<void, Start, End, 0>
+{
+    using type = std::false_type;
+};
+
+template <int Start, int End, int Step = 1>
+using make_sequence = typename make_sequence_impl<void, Start, End, Step>::type;
+
+
+// ========= sequence entry
+template <int I, int... S>
+constexpr int sequence_entry(std::integer_sequence<int, S...>)
+{
+    static_assert(I >= 0 && I < sizeof...(S), "Index out of bounds");
+    auto seq = std::array<int, sizeof...(S)>{S...};
+    return std::get<I>(seq);
+}
+
+template <int ... S>
+constexpr int sequence_entry(std::integer_sequence<int, S...>, int index)
+{
+    if (index < 0 || index >= sizeof...(S))
+    {
+		throw std::runtime_error("Index out of bounds");
+    }
+	auto seq = std::array<int, sizeof...(S)>{S...};
+    return seq[index];
+}
+
+#endif
---
+++ algos/integer_sequence/main.cpp
@@ -0,0 +1,17 @@
+#include "is.hpp"
+
+int main()
+{
+    auto seq0 = std::integer_sequence<int, 0, 5, 8>{};
+    std::cout << seq0 << std::endl; 
+    auto seq1 = make_sequence<5, 10, 2>();
+    auto seq2 = make_sequence<10, 1, -2>();
+    std::cout << seq1 << std::endl; // <5, 7, 9>
+    std::cout << seq2 << std::endl; // <10, 8, 6, 4, 2>
+    
+    std::cout << sequence_entry<1>(seq1) << std::endl; // 7
+    //std::cout << sequence_entry<3>(seq1) << std::endl; // compile error
+    
+    std::cout << sequence_entry(seq1, 1) << std::endl; // 7
+    //std::cout << sequence_entry(seq1, 3) << std::endl; // runtime error
+}
