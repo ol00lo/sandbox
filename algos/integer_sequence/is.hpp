8fb6dee207bcee735de3b0d102a59522122cd3f7
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2025-02-27 23:28:47 +0300

    .

--- algos/integer_sequence/is.hpp
+++ algos/integer_sequence/is.hpp
@@ -1,26 +1,17 @@
 #ifndef IS_HPP
 #define IS_HPP
 
-#include <iostream>
 #include <array>
+#include <iostream>
 #include <utility>
 
-// ========= operator<<
-template <typename T, T... I>
-std::ostream& operator<<(std::ostream& os, const std::integer_sequence<T, I...>&)
+namespace impl
 {
-    os << "< ";
-    ((os << I <<  " "), ...);
-    os << ">";
-    return os;
-}
-
-// ========= make sequence
 template <int Start, int End, int Step>
-using is_valid = typename std::enable_if<(Step>0 ? Start<=End : End<=Start)>::type;
+using is_valid = typename std::enable_if<(Step > 0 ? Start < End : End < Start)>::type;
 
 template <int Start, int End, int Step>
-using is_end = typename std::enable_if<(Step>0 ? Start>End : End>Start)>::type;
+using is_end = typename std::enable_if<(Step > 0 ? Start >= End : End >= Start)>::type;
 
 template <int Step>
 using is_step = typename std::enable_if<Step == 0>::type;
@@ -30,8 +21,7 @@ struct make_sequence_impl;
 
 template <int Start, int End, int Step, int... Indices>
 struct make_sequence_impl<is_valid<Start, End, Step>, Start, End, Step, Indices...>
-    : make_sequence_impl<void, Start + Step, End, Step, Indices..., Start>
-{};
+    : make_sequence_impl<void, Start + Step, End, Step, Indices..., Start> {};
 
 template <int Start, int End, int Step, int... Indices>
 struct make_sequence_impl<is_end<Start, End, Step>, Start, End, Step, Indices...>
@@ -44,10 +34,23 @@ struct make_sequence_impl<void, Start, End, 0>
 {
     using type = std::false_type;
 };
+} // namespace impl
+
+
+// ========= operator<<
+template <typename T, T... I>
+std::ostream& operator<<(std::ostream& os, const std::integer_sequence<T, I...>&)
+{
+    os << "< ";
+    ((os << I << " "), ...);
+    os << ">";
+    return os;
+}
+
+// ========= make sequence
 
 template <int Start, int End, int Step = 1>
-using make_sequence = typename make_sequence_impl<void, Start, End, Step>::type;
-
+using make_sequence = typename impl::make_sequence_impl<void, Start, End, Step>::type;
 
 // ========= sequence entry
 template <int I, int... S>
@@ -58,14 +61,14 @@ constexpr int sequence_entry(std::integer_sequence<int, S...>)
     return std::get<I>(seq);
 }
 
-template <int ... S>
+template <int... S>
 constexpr int sequence_entry(std::integer_sequence<int, S...>, int index)
 {
     if (index < 0 || index >= sizeof...(S))
     {
-		throw std::runtime_error("Index out of bounds");
+        throw std::runtime_error("Index out of bounds");
     }
-	auto seq = std::array<int, sizeof...(S)>{S...};
+    auto seq = std::array<int, sizeof...(S)>{S...};
     return seq[index];
 }
 
