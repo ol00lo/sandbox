a0ae6ca2f1b04758aea4a001d5840c6f0ec0eb20
ol00lo <115536900+ol00lo@users.noreply.github.com>
2025-02-28 09:37:17 +0300

    [LIZA-125] Номера с заданной суммой (#40)

---
+++ QT/rightcontrol.py
@@ -0,0 +1,60 @@
+from PyQt6 import QtWidgets, QtCore, QtGui
+
+class RightControl(QtWidgets.QWidget):
+    _triggered = QtCore.pyqtSignal()
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setLayout(QtWidgets.QVBoxLayout())
+        
+        # create lineedit
+        self._lineedit1 = QtWidgets.QLineEdit(self)
+        self._lineedit1.setPlaceholderText("sum of digits")
+        self._lineedit1.setMinimumHeight(50)  
+        self._label1 = QtWidgets.QLabel("write sum of digits:", self)
+
+        # create radio buttons
+        self.rb3 = QtWidgets.QRadioButton("3", self)
+        self.rb4 = QtWidgets.QRadioButton("4", self)
+        self.rb3.setMinimumHeight(50)
+        self.rb4.setMinimumHeight(50)
+        self.rb3.setChecked(True)
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addWidget(self.rb3)
+        button_layout.addWidget(self.rb4)
+        self._label2 = QtWidgets.QLabel("choose num of digits:", self)
+
+        # create start button
+        button = QtWidgets.QPushButton("RUN")
+        button.setMinimumHeight(50)
+        button.clicked.connect(lambda: self._triggered.emit())
+
+        # set font
+        font = QtGui.QFont()
+        font.setPointSize(15)  
+        self._lineedit1.setFont(font)
+        self.rb3.setFont(font)
+        self.rb4.setFont(font)
+        font2 = QtGui.QFont()
+        font2.setPointSize(12)
+        self._label1.setFont(font2)
+        self._label2.setFont(font2)
+
+        # add on layout
+        self.layout().addWidget(self._label1) 
+        self.layout().addWidget(self._lineedit1)
+        self.layout().addWidget(self._label2)
+        self.layout().addLayout(button_layout)
+        self.layout().addWidget(button)
+        self.layout().addItem(QtWidgets.QSpacerItem(50, 50, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding))
+    
+    def get_data(self):
+        x = self._lineedit1.text()
+        try:
+            x = int(x)
+        except Exception as e:
+            raise Exception("Enter numbers only")
+        if x < 0:
+            raise Exception("Sum of digits must be positive")
+        y = 3 if self.rb3.isChecked() else 4
+        return x, y
