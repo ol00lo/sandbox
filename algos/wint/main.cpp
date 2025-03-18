65d116510569790385d06e2c0a688f8486cb7684
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2025-03-18 13:33:38 +0300

    LIZA-126: Целые числа произвольной битности

---
+++ algos/wint/main.cpp
@@ -0,0 +1,100 @@
+﻿#include "wint.hpp"
+#include <iomanip>
+
+void print()
+{
+    WInt<32, false> i(4294967295);
+    std::cout << "decimal: " << i << std::endl;
+    std::cout << "binary: " << i.print_binary() << std::endl;
+    WInt<32> j(-2'147'483'648);
+    std::cout << "decimal: " << j << std::endl;
+    std::cout << "binary: " << j.print_binary() << std::endl;
+
+    WInt<16, false> k(65535);
+    std::cout << "decimal: " << k << std::endl;
+    std::cout << "binary: " << k.print_binary() << std::endl;
+
+}
+
+void arithmetic()
+{
+    WInt<8, false> x(3);
+    WInt<8, false> y(5);
+
+    x += y;
+    std::cout << "8 " << "  ==  " << x << std::endl;
+    x -= y;
+    std::cout << "3 " << "  ==  " << x << std::endl;
+    x *= y;
+    std::cout << "15" << "  == " << x << std::endl;
+    x /= y;
+    std::cout << "3 " << "  ==  " << x << std::endl;
+
+    WInt<8> l(3);
+    WInt<8> k(-3);
+    std::cout << std::setw(5) << "l " << "  ==  " << l << std::endl;
+    std::cout << std::setw(5) << "k " << "  ==  " << k << std::endl;
+    l += k;
+    std::cout << std::setw(5) << "l+=k" << "  ==  " << l << std::endl;
+    k += k;
+    std::cout << std::setw(5) << "k+=k" << "  ==  " << k << std::endl;
+    l *= k;
+    std::cout << std::setw(5) << "l*=k" << "  ==  " << l << std::endl;
+    k *= k;
+    std::cout << std::setw(5) << "k*=k" << "  ==  " << k << std::endl;
+}
+
+void inequalities()
+{
+    WInt<8, false> x(3);
+    WInt<8, false> y(5);
+    WInt<8, false> z(5);
+
+    std::cout << std::setw(5) << "true" << " ==  " << (x < y) << std::endl;
+    std::cout << std::setw(5) << "true" << " ==  " << (y > x) << std::endl;
+    std::cout << std::setw(5) << "false" << " ==  " << (y > z) << std::endl;
+    std::cout << std::setw(5) << "false" << " ==  " << (y < z) << std::endl;
+    std::cout << std::setw(5) << "true" << " ==  " << (y <= z) << std::endl;
+    std::cout << std::setw(5) << "true" << " ==  " << (y == z) << std::endl;
+
+    WInt<8> l(3);
+    WInt<8> k(-3);
+    std::cout << std::setw(5) << "true" << " ==  " << (k < l) << std::endl;
+}
+
+void cast()
+{
+    WInt<16> a(270);
+    WInt<8> b(a);
+    std::cout << a.print_binary() << " - NBits= 16" << std::endl << "           "
+              << b.print_binary() << " - NBits = 8" << std::endl;
+    WInt<32> c(b);
+    WInt<32> d(a);
+    std::cout << std::setw(5) << "false" << " ==  " << (c == d) << std::endl;
+
+    WInt<16, false> e(a);
+    WInt<16, false> f(b);
+
+    std::cout << a.print_binary() << " - 16, true\n" << e.print_binary() << " - 16, false\n";
+    std::cout << "           " << b.print_binary() << " -  8, true\n" << f.print_binary() << " - 16, false\n";
+
+    WInt<8, false> g(255);
+    std::cout << "  g  = " << g << " = <8, false> " << g.print_binary() << std::endl;
+    WInt<8, true> h(g);
+    std::cout << "h(g) = " << h << " = <8, true > " << h.print_binary() << std::endl;
+}
+
+int main()
+{
+    std::cout << "---------- PRINT TEST ----------\n";
+    print();
+
+    std::cout << "\n---------- ARITHMETIC TEST ----------\n";
+    arithmetic();
+
+    std::cout << "\n---------- INEQUALITIES TEST ----------\n";
+    inequalities();
+
+    std::cout << "\n---------- CAST TEST ----------\n";
+    cast();
+}
---
+++ algos/wint/wint.hpp
@@ -0,0 +1,214 @@
+﻿#ifndef WINT_H
+#define WINT_H
+#include <bitset>
+#include <iostream>
+#include <string>
+
+template <int NBits, bool Signed = true>
+class WInt
+{
+public:
+    WInt(int n = 0)
+    {
+        data_ = n;
+    }
+
+    template <int NBits2, bool Signed2>
+    WInt(const WInt<NBits2, Signed2>& other)
+    {
+        auto othdata = other.get_data();
+
+        for (int i = 0; i < NBits; i++)
+        {
+            data_[i] = (i < NBits2) ? othdata[i] : 0;
+        }
+
+        if (Signed && !Signed2)
+        {
+            data_[NBits - 1] = 0;
+        }
+    }
+
+    WInt& operator+=(const WInt& other)
+    {
+        std::bitset<NBits> plus1 = other.data_;
+        data_ = sum(data_, plus1, 0);
+        return *this;
+    }
+
+    WInt& operator-=(const WInt& other)
+    {
+        std::bitset<NBits> plus1 = other.data_;
+        data_ = sum(data_, plus1, 1);
+        return *this;
+    }
+
+    WInt& operator*=(const WInt& other)
+    {
+        std::bitset<NBits> result(0);
+        std::bitset<NBits> temp = data_;
+
+        for (size_t i = 0; i < NBits; ++i)
+        {
+            if (other.data_[i])
+            {
+                result = sum(result, temp << i, 0);
+            }
+        }
+
+        data_ = result;
+        return *this;
+    }
+
+    WInt& operator/=(const WInt& other)
+    {
+        if (other.data_ == 0)
+        {
+            throw std::runtime_error("Division by zero");
+        }
+        std::bitset<NBits> res(0);
+        std::bitset<NBits> rem(0);
+
+        const std::bitset<NBits> one(1);
+        const std::bitset<NBits> zero(0);
+
+        for (int I = NBits - 1; I >= 0; I--)
+        {
+            rem = rem << 1;
+            if (data_[I] == 1)
+                rem = rem | one;
+            else
+                rem = rem | zero;
+
+            if (is_more_then(rem, other.data_, 1))
+            {
+                rem = sum(rem, other.data_, 1);
+                res[I] = 1;
+            }
+        }
+        data_ = res;
+        return *this;
+    }
+
+    bool operator<(const WInt& other) const
+    {
+        return is_more_then(other.data_, data_, 0);
+    }
+    bool operator>(const WInt& other) const
+    {
+        return is_more_then(data_, other.data_, 0);
+    }
+    bool operator<=(const WInt& other) const
+    {
+        return is_more_then(other.data_, data_, 1);
+    }
+    bool operator>=(const WInt& other) const
+    {
+        return is_more_then(data_, other.data_, 1);
+    }
+    bool operator==(const WInt& other) const
+    {
+        return (data_ ^ other.data_) == 0;
+    }
+
+    std::string print_binary()
+    {
+        std::string str = data_.to_string();
+        std::string res = "";
+        for (size_t i = 0; i < str.size(); ++i)
+        {
+            if (i > 0 && (str.size() - i) % 3 == 0)
+            {
+                res += "'";
+            }
+            res += str[i];
+        }
+        return res;
+    }
+
+    friend std::ostream& operator<<(std::ostream& oss, const WInt& x)
+    {
+        long long decimalValue = x.to_decimal();
+
+        std::string str = std::to_string(decimalValue);
+        for (size_t i = 0; i < str.size(); ++i)
+        {
+            if (i > 0 && (str.size() - i) % 3 == 0)
+            {
+                oss << "'";
+            }
+            oss << str[i];
+        }
+        return oss;
+    }
+
+    std::bitset<NBits> get_data() const
+    {
+        return data_;
+    }
+
+private:
+    std::bitset<NBits> data_;
+    std::bitset<NBits> sum(std::bitset<NBits> a, std::bitset<NBits> b, bool is_substract)
+    {
+        std::bitset<NBits> s;
+        while (b != 0)
+        {
+            s = a ^ b;
+            if (is_substract)
+                b = (~a & b) << 1;
+            else
+                b = (a & b) << 1;
+            a = s;
+        }
+        return a;
+    }
+
+    bool compare_positive(const std::bitset<NBits>& a, const std::bitset<NBits>& b, bool need_equal) const
+    {
+        for (int I = NBits - 1; I >= 0; I--)
+        {
+            if (a[I] != b[I])
+            {
+                return a[I] == 1;
+            }
+        }
+        return need_equal;
+    }
+
+    bool is_more_then(const std::bitset<NBits>& a, const std::bitset<NBits>& b, bool need_equal) const
+    {
+        bool is_a_negative = a[NBits - 1];
+        bool is_b_negative = b[NBits - 1];
+
+        if (!is_a_negative && !is_b_negative)
+        {
+            return compare_positive(a, b, need_equal);
+        }
+        else if (is_a_negative && is_b_negative)
+        {
+            return compare_positive(b, a, need_equal);
+        }
+
+        return !is_a_negative;
+    }
+
+    long long to_decimal() const
+    {
+        long long result = 0;
+        for (int i = 0; i < NBits; ++i)
+        {
+            if (data_[i])
+            {
+                result += (1LL << i);
+            }
+        }
+        if (Signed && data_[NBits - 1])
+        {
+            result -= (1LL << NBits);
+        }
+        return result;
+    }
+};
+
+#endif
