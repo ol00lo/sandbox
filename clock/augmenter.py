df045c77608405fa569b2ccdd29b093a6083afe4
yatracker-ol00lo <yatracker.ol00lo@yandex.ru>
2025-04-09 15:30:51 +0300

    LIZA-113: TimeRecognizer: обучить первый вариант

--- clock/augmenter.py
+++ clock/augmenter.py
@@ -6,18 +6,48 @@ np.bool = bool
 
 def get_augmenter(level):
     if level == 0:
-        return iaa.Sometimes(0.95,  
-                iaa.SomeOf((1, None), [
-                iaa.Affine(rotate=(-5, 5), mode='reflect'),  
-                iaa.Multiply((0.6, 1.2)),
+        print("im here")
+        return iaa.Sequential([
+            iaa.Sometimes(0.7, iaa.Affine(
+                rotate=(-3, 3),
+                mode='reflect'
+            )),
+
+            iaa.Sometimes(0.5, iaa.WithHue(
+                iaa.Add((-10, 10))  # Минимальное изменение оттенка
+            )),
+
+            iaa.Sometimes(0.5, iaa.Affine(
+                scale=(0.98, 1.02)
+            )),
+
+            iaa.Sometimes(0.3, iaa.OneOf([
+                iaa.GaussianBlur(sigma=(0, 0.3)),
+                iaa.AdditiveGaussianNoise(scale=(0, 0.02*255))
+            ]))
+        ], random_order=True)
+    elif level == 1:
+        return iaa.Sometimes(0.8,  # Немного уменьшили вероятность аугментации
+                iaa.SomeOf((1, 3), [  # Ограничили максимальное число преобразований
+                # Более мягкое аффинное преобразование
+                iaa.Affine(
+                    rotate=(-5, 5),
+                    scale=(0.95, 1.05),  # Добавили небольшое масштабирование
+                    mode='reflect'
+                ),
                 iaa.OneOf([
-                    iaa.GaussianBlur(sigma=(0, 0.7)),  
-                    iaa.AverageBlur(k=(2, 5)),
+                    iaa.GaussianBlur(sigma=(0, 0.5)),
+                    iaa.MedianBlur(k=(1, 3)),
                 ]),
-                iaa.AddToHue((-50, 50)),
+                # Альтернатива AddToHue с сохранением яркости
+                iaa.WithBrightnessChannels(
+                    iaa.Add((-20, 20))
+                ),
+                # Добавили сохранение белых областей
+                iaa.Sometimes(0.3, iaa.HistogramEqualization())
             ], random_order=True))
         
-    elif level == 1:
+    elif level == 2:
         return iaa.Sometimes(0.95, 
             iaa.SomeOf((2, None), [
                 iaa.Affine(rotate=(-5, 5), mode='reflect'), 
