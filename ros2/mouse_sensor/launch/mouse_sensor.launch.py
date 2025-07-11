e85c8a2d506553d8dccfcaec7db2f396d2f9691a
ol00lo <yatracker.ol00lo@yandex.ru>
2025-07-11 11:06:20 +0300

    .

--- ros2/mouse_sensor/launch/mouse_sensor.launch.py
+++ ros2/mouse_sensor/launch/mouse_sensor.launch.py
@@ -5,17 +5,17 @@ import os
 
 def generate_launch_description():
     sensor_path = os.path.join(
-        get_package_share_directory('mouse_control'),
+        get_package_share_directory('mouse_sensor'),
         'config', 'config.yaml'
     )
     saver_path = os.path.join(
-        get_package_share_directory('mouse_saver'),
+        get_package_share_directory('sensor_saver'),
         'config', 'config.yaml'
     )
 
     return LaunchDescription([
         Node(
-            package='mouse_control',
+            package='mouse_sensor',
             executable='mouse_sensor',
             name='mouse_sensor',
             parameters=[sensor_path],
@@ -23,9 +23,9 @@ def generate_launch_description():
             output='screen'
         ),
         Node(
-            package='mouse_saver',
-            executable='mouse_saver',
-            name='mouse_saver',
+            package='sensor_saver',
+            executable='sensor_saver',
+            name='sensor_saver',
             parameters=[saver_path],
             arguments=['--ros-args', '--log-level', 'debug'],
             output='screen'
