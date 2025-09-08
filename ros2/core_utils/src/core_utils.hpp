835a0c903b0015bd0593a51c86cd1fc54ce91c09
ol00lo <yatracker.ol00lo@yandex.ru>
2025-09-08 11:16:02 +0300

    LIZA-149: Обработка ошибок в fmt::format

--- ros2/core_utils/src/core_utils.hpp
+++ ros2/core_utils/src/core_utils.hpp
@@ -1,48 +1,51 @@
 #ifndef CORE_UTILS_HPP
 #define CORE_UTILS_HPP
 
-#include <string>
-#include <string_view>
 #include <fmt/core.h>
 #include <fmt/format.h>
-#include <stdexcept>
 #include <sstream>
+#include <string>
+#include <string_view>
+
 
 template <typename T>
-std::string arg_to_string(const T& arg) {
+std::string arg_to_string(const T& arg)
+{
     std::ostringstream oss;
     oss << arg;
     return oss.str();
 }
 
 template <typename Char, typename T>
-std::string arg_to_string(const fmt::detail::named_arg<Char, T>& arg) {
+std::string arg_to_string(const fmt::detail::named_arg<Char, T>& arg)
+{
     std::ostringstream oss;
     oss << "{\"" << arg.name << "\" => " << arg.value << "}";
     return oss.str();
 }
 
 template <typename... Args>
-std::string make_error_message(std::string_view format_str, Args&&... args) {
+std::string make_error_message(std::string_view format_str, Args&&... args)
+{
     std::ostringstream oss;
-    oss << "Failed to perform format substitution using the format string \""
-        << format_str << "\" and the placeholders: ";
+    oss << "Failed to perform format substitution using the format string \"" << format_str
+        << "\" and the placeholders: ";
 
     std::size_t n = 0;
     ((oss << (n++ > 0 ? ", " : "") << arg_to_string(args)), ...);
-    
+
     return oss.str();
 }
 
 template <typename... Args>
-std::string fmt_format(std::string_view format_str, Args&&... args) {
-    try {
-        return fmt::vformat(
-            fmt::string_view(format_str.data(), format_str.size()),
-            fmt::make_format_args(std::forward<Args>(args)...)
-        );
+std::string fmt_format(std::string_view format_str, Args&&... args)
+{
+    try
+    {
+        return fmt::vformat(format_str, fmt::make_format_args(args ...));
     } 
-    catch (const fmt::format_error& e) {
+    catch (const fmt::format_error& e)
+    {
         auto error_msg = make_error_message(format_str, std::forward<Args>(args)...);
         throw std::runtime_error(error_msg);
     }
--- ros2/core_utils/test/test_fmt_format.cpp
+++ ros2/core_utils/test/test_fmt_format.cpp
@@ -1,20 +1,77 @@
+#include "core_utils.hpp"
+#include <fmt/format.h>
 #include <gtest/gtest.h>
 #include <stdexcept>
 #include <string>
-#include "core_utils.hpp"
-#include <fmt/format.h>
 
-TEST(CoreUtilsFmtFormat, FormatsOnSuccess) {
-    EXPECT_EQ(1*1,  1);
+TEST(CoreUtils, ArgsToStringTest)
+{
+    EXPECT_EQ(arg_to_string(42), std::string("42"));
+    EXPECT_EQ(arg_to_string(12.2), std::string("12.2"));
+    EXPECT_EQ(arg_to_string("a"), "a");
+    auto a = fmt::arg("x", 12);
+    EXPECT_EQ(arg_to_string(a), std::string("{\"x\" => 12}"));
+    auto a1 = fmt::arg("x", 12.2);
+    EXPECT_EQ(arg_to_string(a1), std::string("{\"x\" => 12.2}"));
 }
 
-TEST(CoreUtilsFmtFormat, TryFunction) {
+TEST(CoreUtils, MakeErrorTest)
+{
+    std::string msg = make_error_message("{} {}", 1, 2);
+    EXPECT_NE(msg.find("Failed to perform format substitution"), std::string::npos);
+    EXPECT_NE(msg.find("\"{} {}\""), std::string::npos);
+    EXPECT_NE(msg.find("1, 2"), std::string::npos);
+}
+
+TEST(FmtFormat, FormatsOnSuccess)
+{
     EXPECT_EQ(fmt_format("Hello {} {}", "World", 123), "Hello World 123");
     EXPECT_EQ(fmt_format("Number: {}", 42), "Number: 42");
     EXPECT_EQ(fmt_format("Pi: {:.2f}", 3.14159), "Pi: 3.14");
+    auto result = fmt_format("Hello {name} {value} {name}", fmt::arg("name", "Test"), fmt::arg("value", 456));
+    EXPECT_EQ(result, "Hello Test 456 Test");
 }
 
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST(FmtFormat, FailTests)
+{
+    try
+    {
+        auto result = fmt_format("{} {} {}", 1, 2);
+        FAIL() << "Expected std::runtime_error";
+    } catch (const std::runtime_error& e)
+    {
+        std::string error_msg = e.what();
+        EXPECT_NE(error_msg.find("Failed to perform format substitution"), std::string::npos);
+    }
+
+    try
+    {
+        auto result = fmt_format("Number: {:.2q}", 42);
+        FAIL() << "Expected std::runtime_error";
+    } catch (const std::runtime_error& e)
+    {
+        std::string error_msg = e.what();
+        EXPECT_NE(error_msg.find("Failed to perform format substitution"), std::string::npos);
+    }
+
+    try
+    {
+        auto result = fmt_format("Number: {a}, {b}, {c}", fmt::arg("a", 1), fmt::arg("b", 2), fmt::arg("d", 3));
+        FAIL() << "Expected std::runtime_error";
+    } catch (const std::runtime_error& e)
+    {
+        std::string error_msg = e.what();
+        EXPECT_NE(error_msg.find("Failed to perform format substitution"), std::string::npos);
+    }
+
+    try
+    {
+        auto result = fmt_format("{} {} {b}", 1, 2, fmt::arg("a", 12));
+        FAIL() << "Expected std::runtime_error";
+    } catch (const std::runtime_error& e)
+    {
+        std::string error_msg = e.what();
+        EXPECT_NE(error_msg.find("Failed to perform format substitution"), std::string::npos);
+    }
+
 }
